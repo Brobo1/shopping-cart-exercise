b94621f6f5ae7bb6cd393e7d541518a0735d636d
brobo1 <armanahz95@gmail.com>
2024-11-11 14:49:47 +0100

    Add routing with React Router
    Integrated `react-router-dom` to enable routing, wrapped the app in `BrowserRouter`, and added `NavBar`, `HomePage`, and `Shopping` components. Updated package dependencies to include `react-router-dom`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./components/HomePage.jsx";
+import { Shopping } from "./components/Shopping.jsx";
+import { NavBar } from "./components/NavBar.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/shopping"} element={<Shopping />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Shopping.jsx
@@ -0,0 +1,7 @@
+export function Shopping() {
+  return (
+    <>
+      <div>Shopping page</div>
+    </>
+  );
+}
